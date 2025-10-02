96c6312075bc6a4ccfd02a5c3076fe371e48507c
ec1yps_ <ec1yps@yandex.ru>
2025-10-02 17:55:10 +0300

    StudentsController added

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -7,11 +7,11 @@ namespace ContosoUniversity.Models
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
-        public int CourseID { get; set; }
         public int StudentID { get; set; }
+        public int CourseID { get; set; }
         public Grade? Grade { get; set; }
 
-        public Course Course { get; set; }
         public Student Student { get; set; }
+        public Course Course { get; set; }
     }
 }
--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<UniversityContext>
 	(
-		options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+		options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"))
 	);
 
 // Add services to the container.
@@ -20,6 +20,14 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
+
+///////////////////////////////////////////////////////////////////////////
+IServiceScope scope = app.Services.CreateScope();
+IServiceProvider service = scope.ServiceProvider;
+
+UniversityContext context = service.GetRequiredService<UniversityContext>();
+DbInitializer.Initialize(context);
+///////////////////////////////////////////////////////////////////////////
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
